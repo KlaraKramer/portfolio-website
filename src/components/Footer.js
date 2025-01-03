bed49590c414e4dcebeb7a3092036e85d44c6012
KlaraKramer <kramer.k.20@outlook.com>
2025-01-03 10:49:44 +0000

    Update year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer>
-      <p><a href="/">© 2024 Klara Krämer - All Rights Reserved</a></p>
+      <p><a href="/">© 2025 Klara Krämer - All Rights Reserved</a></p>
       {/* <a
         href="https://www.linkedin.com/in/klara-krämer/"
         target="_blank"
